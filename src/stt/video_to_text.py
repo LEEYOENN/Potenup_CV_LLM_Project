c54f9f75999ac140a8073078000b7911b324d474
walker0625 <jeonminwoo0625@gmail.com>
2025-09-12 17:57:43 +0900

    feat : srt에서 키워드 추출 후 상품 매핑 중

--- src/stt/video_to_text.py
+++ src/stt/video_to_text.py
@@ -14,15 +14,14 @@ def srt_timestamp(t: float) -> str:
 
     return f"{hh:02d}:{mm:02d}:{ss:02d}"
 
-def srt_extract(video_source):
+def srt_extract(video_source): # 36dc8bba-c0ae-475c-8402-04a1d716b647.mp4 포맷
     
     model = WhisperModel('small', device='cpu', compute_type='int8')
     segments, info = model.transcribe(str(VIDEO_PATH / video_source), language='ko', vad_filter=False)
 
     CSV_PATH.mkdir(exist_ok=True)
-
-    file_name = str(uuid.uuid4())
-    txt_path = CSV_PATH / f'{file_name}.txt'
+    csv_name = video_source.split('.')[0]
+    txt_path = CSV_PATH / f'{csv_name}.csv'
 
     with open(txt_path, "w", encoding="utf-8") as full_text:
         for segment in segments:   
@@ -30,4 +29,6 @@ def srt_extract(video_source):
             line = segment.text.strip()
             full_text.write(f"{id}, {srt_timestamp(segment.start)}, {srt_timestamp(segment.end)}, {line}\n")
             
-srt_extract('yt.mp4')
+    return csv_name
+            
+srt_extract('36dc8bba-c0ae-475c-8402-04a1d716b647.mp4')
