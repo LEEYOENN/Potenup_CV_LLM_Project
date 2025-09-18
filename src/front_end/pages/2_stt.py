2068285750e26685ea9b5eced70d27f92f19dd2e
walker0625 <jeonminwoo0625@gmail.com>
2025-09-18 11:34:48 +0900

    feat : UI 수정

--- src/front_end/pages/2_stt.py
+++ src/front_end/pages/2_stt.py
@@ -11,20 +11,20 @@ st.markdown("""
 <style>
 /* 파일 업로더 글씨 색상 */
 [data-testid="stFileUploader"] label {
-    color: black !important;
-    font-weight: 600;
+    color: white !important;
+    font-weight: 1000;
 }
 
 /* number_input 레이블 색상 */
 [data-testid="stNumberInputContainer"] label {
-    color: black !important;
-    font-weight: 600;
+    color: white !important;
+    font-weight: 1000;
 }
 
 /* 입력 필드 텍스트 색상 */
 [data-testid="stFileUploader"] div, 
 [data-baseweb="input"] input {
-    color: black !important;
+    color: white !important;
 }
 </style>
 """, unsafe_allow_html=True)
@@ -38,7 +38,7 @@ show_time = st.number_input("광고 노출 시간(초)", min_value=0, step=1)
 # -----------------------------
 # 전송 버튼
 # -----------------------------
-if st.button("전송", type="primary"):
+if st.button("광고 추가 영상으로 변환", type="primary"):
     if not video_file:
         st.warning("영상을 업로드해주세요.")
     else:
@@ -54,9 +54,9 @@ if st.button("전송", type="primary"):
                 res.raise_for_status()
                 data = res.json()
 
-            st.success("서버 응답 성공 ✅")
+            st.success("영상 변환 성공 ✅")
             st.json(data)
 
         except Exception as e:
-            st.error(f"API 호출 실패: {e}")
+            st.error(f"영상 변환 실패: {e}")
 
